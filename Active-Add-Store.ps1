8c7e3b08443d41600fa202fb03b4491dd0a73d3a
dinhkarate <57869841+dinhkarate@users.noreply.github.com>
2024-11-23 07:34:35 +0700

    change web rq to webclient

--- Active-Add-Store.ps1
+++ Active-Add-Store.ps1
@@ -4,20 +4,15 @@ $EnableAddStore = $true
 $WorkFolder = "C:\Temp\SetupFiles"
 if (!(Test-Path $WorkFolder)) { New-Item -ItemType Directory -Path $WorkFolder }
 
-if ($EnableKMSActivation) {
-    Write-Host "Step 1: Kích hoạt bản quyền Windows..."
-    irm https://get.activated.win | iex
-    Write-Host "Đã kích hoạt bản quyền Windows!"
-}
-
 if ($EnableAddStore) {
-    Write-Host "Step 2: Cài đặt Microsoft Store..."
-    $RepoUrl = "https://github.com/kkkgo/LTSC-Add-MicrosoftStore/archive/refs/heads/master.zip"
-    $ZipFile = "$WorkFolder\LTSC-Add-MicrosoftStore.zip"
-    $ExtractPath = "$WorkFolder\LTSC-Add-MicrosoftStore"
+    Write-Host "Step 1: Cài đặt Microsoft Store..."
+    $RepoUrl = "https://codeload.github.com/kkkgo/LTSC-Add-MicrosoftStore/zip/refs/heads/master"
+    $ZipFile = "$env:TEMP\LTSC-Add-MicrosoftStore.zip"
+    $ExtractPath = "$env:TEMP\LTSC-Add-MicrosoftStore"
 
     Write-Host "Đang tải repo từ GitHub..."
-    Invoke-WebRequest -Uri $RepoUrl -OutFile $ZipFile -UseBasicParsing
+    $WebClient = New-Object System.Net.WebClient
+    $WebClient.DownloadFile($RepoUrl, $ZipFile)
     Write-Host "Đã tải repo về: $ZipFile"
 
     Write-Host "Đang giải nén repo..."
@@ -26,8 +21,19 @@ if ($EnableAddStore) {
 
     $AddStoreScript = "$ExtractPath\LTSC-Add-MicrosoftStore-master\Add-Store.cmd"
     Write-Host "Đang thực thi Add-Store.cmd..."
+    $CmdFile = "$ExtractPath\LTSC-Add-MicrosoftStore-master\Add-Store.cmd"
+    (Get-Content $CmdFile) -notmatch 'pause' | Set-Content $CmdFile
+    Write-Host "Đã loại bỏ lệnh 'pause' khỏi $CmdFile."
     Start-Process -FilePath "cmd.exe" -ArgumentList "/c $AddStoreScript" -Wait
-    Write-Host "Đã cài đặt Microsoft Store!"
+    Write-Host "Đã hoàn tất cài đặt Microsoft Store!"
+
+}
+
+if ($EnableKMSActivation) {
+    Write-Host "Step 2: Kích hoạt bản quyền Windows..."
+    Write-Host "Ấn phím 3 sau khi cmd chạy lên để hoàn tất kích hoạt"
+    Start-Process -FilePath "powershell.exe" -ArgumentList "-NoProfile -Command irm https://get.activated.win | iex"
+    Write-Host "Đã kích hoạt bản quyền Windows!"
 }
 
 Write-Host "Hoàn tất quá trình cài đặt!"
